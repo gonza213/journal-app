0f0bd508cd43667c53cbb594f55ee34f681af867
Gonzalo <gonzaa.vidal.18@gmail.com>
2021-12-08 02:31:02 -0300

    Tercer commit

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -4,21 +4,21 @@ export default () => ({
     isLoading: true,
     entries: [
         {
-            id: new Date().getTime(),
+            id: '1',
             date: new Date().toDateString(),
             text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ipsam dolorem placeat, recusandae obcaecati fuga non libero, incidunt quaerat minima similique nihil ratione facere maxime dignissimos provident id temporibus officia?',
             picture: null,
         },
         {
-            id: new Date().getTime() + 1000,
+            id: '2',
             date: new Date().toDateString(),
             text: 'Lorem 2 ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ipsam dolorem placeat, recusandae obcaecati fuga non libero, incidunt quaerat minima similique nihil ratione facere maxime dignissimos provident id temporibus officia?',
             picture: null,
         },
         {
-            id: new Date().getTime() + 2000,
+            id: '3',
             date: new Date().toDateString(),
-            text: 'Lorem 2 ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ipsam dolorem placeat, recusandae obcaecati fuga non libero, incidunt quaerat minima similique nihil ratione facere maxime dignissimos provident id temporibus officia?',
+            text: 'Lorem 3 ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ipsam dolorem placeat, recusandae obcaecati fuga non libero, incidunt quaerat minima similique nihil ratione facere maxime dignissimos provident id temporibus officia? Lorem 3 ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ipsam dolorem placeat, recusandae obcaecati fuga non libero, incidunt quaerat minima similique nihil ratione facere maxime dignissimos provident id temporibus officia?',
             picture: null,
         }
     ],
